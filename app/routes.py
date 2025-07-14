aceb69faf7519d40008caf9fbc9ababf7f76f9e2
ishuesiah <129911713+ishuesiah@users.noreply.github.com>
2025-07-14 12:59:27 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,12 @@ from . import db
 
 bp = Blueprint('main', __name__)
 
+
+@bp.route('/')
+def home():
+    # redirect anyone hitting / → /users
+    return redirect(url_for('main.list_users'))
+
 @bp.route('/users')
 def list_users():
     q = request.args.get('q', '')
